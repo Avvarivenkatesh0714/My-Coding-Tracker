68508f78572a8808f274d26871f221755935f7af
Avvarivenkatesh0714 <avvarivenkatesh0714@gmail.com>
2025-08-04 19:43:58 +0530

    New Problems Added

---
+++ DSA/Arrays Problems/PascalTriangle.java
@@ -0,0 +1,25 @@
+class PascalTriangle {
+    public List<List<Integer>> generate(int n) { // n = numRows
+        List<List<Integer>> ans = new ArrayList<>();
+
+        // Store the entire Pascal's triangle:
+        for (int row = 1; row <= n; row++) {
+            List<Integer> tempLst = new ArrayList<>(); // temporary list
+            for (int col = 1; col <= row; col++) {
+                tempLst.add(nCr(row - 1, col - 1));
+            }
+            ans.add(tempLst);
+        }
+        return ans;
+    }
+
+    public static int nCr(int n, int r) {
+        long res = 1;
+        // calculating nCr:
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
+        }
+        return (int) res;
+    }
+}
