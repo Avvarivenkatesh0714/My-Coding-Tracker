dfc725c83e9681d9abfc3bb9d40ddb3b3f6788b4
Avvarivenkatesh0714 <avvarivenkatesh0714@gmail.com>
2025-11-25 14:41:11 +0530

    Added

---
+++ DSA/Binary Search (1D, 2D Arrays, Search Space)/KthMissingPositiveNumber.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+//Brute Force
+// public class KthMissingPositiveNumber {
+//     public static void main(String[] args) {
+//       int a[] = {4,7,9,10};
+//       int k = 4;
+//       for(int i=0;i<a.length;i++){
+//         if(a[i]<=k) k++;
+//         else break;
+//       }
+      
+//       System.out.println(k);
+//   }
+// }
